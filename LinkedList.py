7fe8a460b42b8ff00bc3eadeb7d3c942380781ca
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-02 14:57:58 +0530

    linked list constructor

--- LinkedList.py
+++ LinkedList.py
@@ -1,6 +1,15 @@
-class LinkedList:
-    def __init__(self, value):
+class Node:
+    def __init__(self,value):
         self.value = value
+        self.next = None
+
+class LinkedList: #linkedlist constructor
+    def __init__(self, value):
+        #create the node
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
 
     def append(self, value):
         pass
@@ -8,3 +17,7 @@ class LinkedList:
         pass
     def insert(self, index, value):
         pass
+
+my_linked_list = LinkedList(4)
+
+print(my_linked_list.head.value)
