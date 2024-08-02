56567e412ac4057b820533b2c7caf96c4e02b166
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-02 14:49:37 +0530

    linked list

---
+++ LinkedList.py
@@ -0,0 +1,10 @@
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+
+    def append(self, value):
+        pass
+    def prepend(self, value):
+        pass
+    def insert(self, index, value):
+        pass
