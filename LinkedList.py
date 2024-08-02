70aebc21031e3aa7794ef9d07d00ccb85312405a
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-02 15:38:13 +0530

    pop method

--- LinkedList.py
+++ LinkedList.py
@@ -27,6 +27,27 @@ class LinkedList: #linkedlist constructor
             self.tail = new_node
         self.length += 1
         return True
+    
+    def pop(self):
+        if self.head is None:
+            return None
+        
+        temp = self.head
+        pre = self.head
+
+        while(temp.next): # moves the pre and temp in the list
+            pre = temp
+            temp = temp.next
+
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+
+        return temp.value
 
     def prepend(self, value):
         pass
@@ -36,4 +57,11 @@ class LinkedList: #linkedlist constructor
 my_linked_list = LinkedList(4)
 my_linked_list.append(2)
 
+# my_linked_list.print_list()
+
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+
+print("List items are: ")
 my_linked_list.print_list()
