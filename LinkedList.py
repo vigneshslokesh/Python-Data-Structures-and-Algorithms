159adc5fb1e4f1d429a3c877ba08fcba4d5b7374
vigneshslokesh <119556220+vigneshslokesh@users.noreply.github.com>
2024-08-12 23:46:11 +0530

    LL

--- LinkedList.py
+++ LinkedList.py
@@ -105,3 +105,15 @@ my_linked_list.print_list()
 
 print("\nGet method: ")
 print(my_linked_list.get(1))
+
+
+# def remove_last_node(self):
+
+#     if self.head is None:
+#         return
+
+#     curr_node = self.head
+#     while (curr_node.next != None and curr_node.next.next != None):
+#         curr_node = curr_node.next
+
+#     curr_node.next = None
