2841880c3cf28b4d884fb43e0bb9e5b9de4a68f2
vigneshslokesh <vigneshslokesh0612@gmail.com>
2025-05-01 20:04:09 +0530

    Insert Method

--- LinkedList1.py
+++ LinkedList1.py
@@ -89,17 +89,32 @@ class LinkedList:
             return True
         return False
 
+# Insert Method
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index -1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
 
     
-my_linked_list = LinkedList(11)
+my_linked_list = LinkedList(0)
 
-my_linked_list.append(3)
+my_linked_list.append(2)
 my_linked_list.append(23)
-my_linked_list.append(7)
 
-my_linked_list.set_value(1,4)
 my_linked_list.print_list()
 
+my_linked_list.insert(2,4)
+
+my_linked_list.print_list()
 
 
 
